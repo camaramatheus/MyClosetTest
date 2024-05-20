cf0d66115a29d42c7ff679fe97a1a0619cbfe604
Matheus Câmara <104511336+camaramatheus@users.noreply.github.com>
2024-05-20 17:00:55 -0300

    Add files via upload

---
+++ mycloset/client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold  mb-4 text-slate-800'>
+        Welcome to my Auth App!
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        
+      </p>
+      <p className='mb-4 text-slate-700'>
+        
+      </p>
+      <p className='mb-4 text-slate-700'>
+        
+      </p>
+    </div>
+  );
+}
